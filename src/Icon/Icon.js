b57f18d2ba0eb2a171a5f5fb2cc5462f958f4807
Kristie Giles <kristiegiles@Kristies-MacBook-Pro.local>
2017-08-09 13:05:03 -0600

    aria hidden

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -14,7 +14,7 @@ const Icon = props => {
 		<svg className={classnames(`icon icon--${props.slug}`, props.className)}
 			xmlns="http://www.w3.org/2000/svg"
 			viewBox="0 0 24 24"
-			aria-labelledby="title"
+			aria-hidden="true"
 			dangerouslySetInnerHTML={{__html: iconMap[props.slug]}}/>
 	)
 }
