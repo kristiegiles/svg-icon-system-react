ad79a98e18c18f9c0fcd5c6e0a643f9da9808577
Kristie Giles <kristiegiles@Kristies-MacBook-Pro.local>
2017-08-09 13:00:33 -0600

    changing to functional components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Icon from './Icon/Icon'
+import './App.css';
+
+const App = props => {
+  return (
+    <div className="App">
+      <h1><Icon className="u-margin-right" slug="check"/>React SVG Icon System Demo</h1>
+      <Icon slug="hamburger"/>
+      <Icon slug="warning" className="icon--red"/>
+      <Icon slug="pin"/>
+      <Icon slug="x"/>
+      <button>Search<Icon className="u-margin-left" slug="search"/></button>
+      <button>Next<Icon slug="chevron" className="u-margin-left icon--flipped"/></button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Icon/Icon.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classnames from 'classnames';
+import './Icon.css';
+
+let fileList = require.context('./icons', true, /[\s\S]*$/);
+let iconMap = {};
+fileList.keys().forEach(fileName => {
+	fileName = fileName.replace('./', '');
+	iconMap[fileName.replace('.svg', '')] = require(`./icons/${fileName}`);
+});
+
+const Icon = props => {
+	return (
+		<svg className={classnames(`icon icon--${props.slug}`, props.className)}
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			aria-labelledby="title"
+			dangerouslySetInnerHTML={{__html: iconMap[props.slug]}}/>
+	)
+}
+
+export default Icon
